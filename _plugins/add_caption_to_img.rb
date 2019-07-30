f4ad4f7c3f4a29de88f645b9c73cbc90f43d22cd
chongjun.lei <chongjun.lei@neophotonics.com>
2019-07-30 13:28:35 +0800

    mod plugin

--- _plugins/add_caption_to_img.rb
+++ _plugins/add_caption_to_img.rb
@@ -1,8 +1,12 @@
-Jekyll::Hooks.register :posts, :pre_render do |post, payload|
-    docExt = post.extname.tr('.', '')
-    # only process if we deal with a markdown file
-    if payload['site']['markdown_ext'].include? docExt
-      newContent = post.content.gsub(/\!\[(.+)\]\(([^\?]+)\??(\w*)\)/, '<div class="post-img-wrapper \3"><img src="\2" alt="\1"><div class="img-caption">\1</div></div>')
-      post.content = newContent
-    end
+
+class AddCaptionToImage < Jekyll::Converter
+  priority :high
+
+  def matches(ext)
+    ext.downcase == ".md"
   end
+
+  def convert(content)
+    content.gsub(/\!\[(.+)\]\(([^\?]+)\??(\w*)\)/, '<div class="post-img-wrapper \3"><img src="\2" alt="\1"><div class="img-caption">\1</div></div>')
+  end
+end
